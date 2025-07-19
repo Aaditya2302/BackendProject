397dbe3127f3253f9cb8983a57a6a1b1304052d5
Aaditya2302 <aadityaaggarwal475@gmail.com>
2025-07-19 18:43:39 +0530

    addrefreshToken endpoint

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -56,12 +56,12 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.passwor);
+  return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken -
+userSchema.methods.generateAccessToken =
   function () {
-    jwt.sign({
+    return jwt.sign({
       _id: this._id,
       email: this.email,
       username: this.username,
@@ -73,8 +73,8 @@ userSchema.methods.generateAccessToken -
     }
     );
   };
-userSchema.methods.generateRefreshToken - function () {
-    jwt.sign({
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
       _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
